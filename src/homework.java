f867f3bc7f44803370a9160660f4f449864daac5
SofiHayrapetyann <sofi.hayrapetian@gmail.com>
2023-01-06 21:54:17 +0400

    add homework1

--- src/homework.java
+++ src/homework.java
@@ -1,21 +1,21 @@
-public class Main {
-    public static void main(String[] args){
-    int  a=4;
-    int  b=6;
-    int c;
-    c =a*a+b*b;
-    System.out.println("c= " +c);
+public class homework {
 
+        public static void main(String[] args){
+            int  a=4;
+            int  b=6;
+            int c;
+            c =a*a+b*b;
+            System.out.println("c= " +c);
+            a=7;
+            b=9;
+            c=8;
+            int S =(a+b)/2*c;
+            System.out.println("S= "+S);
 
-    a=7;
-    b=9;
-    c=8;
-    int S =(a+b)/2*c;
-    System.out.println("S= "+S);
+            int n=134;
+            System.out.println(n/100);
+            System.out.println(n/10%10);
+            System.out.println(n%10);
+        }
+    }
 
-    int n=134;
-    System.out.println(n/100);
-    System.out.println(n/10%10);
-    System.out.println(n%10);
-}
-}
