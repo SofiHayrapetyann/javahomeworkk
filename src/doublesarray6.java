e91c9c7c28c700f6be8bbd15ffdda3f7b30cbc40
SofiHayrapetyann <sofi.hayrapetian@gmail.com>
2023-01-10 12:07:57 +0400

    Add homework6

---
+++ src/doublesarray6.java
@@ -0,0 +1,26 @@
+public class doublesarray6 {
+    public static void main(String[] args) {
+
+            double[] arr1 = {13, -8.66, 3, 4.5, -5};
+            double[] arr2 = new double[5];
+            int i,
+        if (arr1[0] > 0) {
+            arr2[i] = arr1[4];
+        }
+        if (arr1[1] > 0) {
+            arr2[i] = arr1[1];
+        }
+        if (arr1[2] > 0) {
+            arr2[i] = arr1[2];
+        }
+        if (arr1[3] > 0) {
+            arr2[i] = arr1[3];
+        }
+        if (arr1[4] > 0) {
+            arr2[i] = arr1[4];
+        }
+    }
+}
+
+
+
