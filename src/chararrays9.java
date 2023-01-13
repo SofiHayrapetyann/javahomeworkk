49a3c8eb80f397cad369901271d037d9295a0c0b
SofiHayrapetyann <sofi.hayrapetian@gmail.com>
2023-01-13 04:50:22 +0400

    add homework9

--- src/chararrays9.java
+++ src/chararrays9.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class chararrays9 {
+    public static void main(String[] args) {
+        int i = 0;
+        char[] letters = {'a', 'b', 'A', 'e', 'f', 'g'};
+        Scanner b = new Scanner(System.in);
+        System.out.println("Enter the letter");
+        char letter = b.next().charAt(0);
+        while (letter != letters[i]){
+            i++;
+            if (i >= letters.length) {
+                System.out.println("The index is invalid");
+            break;
+            }
+    }
+        System.out.println(+i);
 }
+}
+
