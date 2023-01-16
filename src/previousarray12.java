ee9f5f3e62403140d5f7980f79a07e2af0b5907c
SofiHayrapetyann <sofi.hayrapetian@gmail.com>
2023-01-16 04:53:05 +0400

    add homework11 homeowrk12

---
+++ src/previousarray12.java
@@ -0,0 +1,19 @@
+public class previousarray12 {
+    public static void main(String[] args) {
+        int[] array = {6, 13, 11, 21, 69, 66};
+        int count = 0;
+        int k = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] < array[i + 1]) {
+                count++;
+            }
+            }
+
+                System.out.println(count);
+            }
+        }
+
+
+
+
+
