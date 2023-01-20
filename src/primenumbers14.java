31017cd7d56f4c0ed5656a6e9db0d12a77202af2
SofiHayrapetyann <sofi.hayrapetian@gmail.com>
2023-01-20 04:47:45 +0400

    add homework14

--- src/primenumbers14.java
+++ src/primenumbers14.java
@@ -1,19 +1,19 @@
 public class primenumbers14 {
     public static void main(String[] args) {
-        int[] array = {5, 2, 6, 13, 16, 18, 22, 23};
+        int[] array = {5, 6, 13, 19, 16, 18,55,22, 23};
         int count = 0;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 != 0) {
-                count++;
-                System.out.println(array[i] + " ");
+            int b = 2;
+            while (array[i] / 2 >= b) {
+                if (array[i] % b == 0) {
+                    break;
+                } else if (array[i] % b !=0) {
+                    count++;
+                    System.out.println(array[i]);
+               }
+           b++; }
             }
-            if (array[i] == 2) {
-                System.out.println(2);
-            }
-        }
         if (count == 0) {
-            System.out.println("There is no prime number");
-        }
-    }
-}
+        System.out.println("There is no prime number");
+    }} }
 
