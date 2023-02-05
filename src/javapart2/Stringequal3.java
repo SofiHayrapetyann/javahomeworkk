3860c7c910afebe2e12de101a5964f78d9311915
SofiHayrapetyann <sofi.hayrapetian@gmail.com>
2023-02-05 20:00:24 +0400

    add homework 3

---
+++ src/javapart2/Stringequal3.java
@@ -0,0 +1,46 @@
+package javapart2;
+
+import java.util.Arrays;
+
+public class Stringequal3 {
+    public static void main(String[] args) {
+        String text = ("red yellow blue brown string yellow");
+        String word= ("yellow");
+        System.out.println(stringequals1(text,word));
+        System.out.println(stringequal2(text));
+
+    }
+    static int stringequals1(String text, String word) {
+        int count = 0;
+        for (String text1 : text.split(" ")) {
+            if (text1.equals(word)) {
+                count++;
+            }
+        }
+        if (count == 0) {
+            return -1;
+        } else {
+            if (text.length() == 0 || word.length() == 0) {
+                return -2;
+            }
+            return count;
+        }
+    }
+
+static int stringequal2(String text){
+        int count=0;
+    for (String text1 : text.split(" ")) {
+        if (text1.equals("string")) {
+            count++;
+        }
+    }
+    if (count == 0) {
+        return -1;
+    } else {
+        if (text.length() == 0) {
+            return -2;
+        }
+        return count;
+    }
+
+}}
