aa9a521f74e318d990c5cb9744ed11c6e25481bd
SofiHayrapetyann <sofi.hayrapetian@gmail.com>
2023-02-14 01:38:49 +0400

    add homework 7

---
+++ src/Homework67/Homework7.java
@@ -0,0 +1,19 @@
+package Homework67;
+class GasStove {
+    int electric1;
+    int electric2;
+    int gas1;
+    int gas2;
+    int gas3;
+     GasStove (boolean a, Boolean b, boolean c ){
+
+     }
+
+}
+  public class Homework7 {
+
+
+}
+
+
+
