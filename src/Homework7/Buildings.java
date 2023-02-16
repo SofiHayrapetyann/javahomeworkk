7472d3ae6b27f4883880da215189428433350363
SofiHayrapetyann <sofi.hayrapetian@gmail.com>
2023-02-17 01:41:29 +0400

    add homework7

---
+++ src/Homework7/Buildings.java
@@ -0,0 +1,13 @@
+package Homework7;
+class Building {
+    int buildingFloors= 4;
+    int [] apartmentof2room= {1,3,5,6,8,10,11,13,15,16,18,20};
+    int [] apartmentof3room={2,7,12,17};
+
+}class District {
+    int [] building;
+    int park;
+}
+public class Buildings {
+
+}
